5e1c159099e3fc9529f8ef7ac0fb7910669710b1
Emmanuele <emmanuele.salvati@gmail.com>
2014-05-29 15:15:24 -0400

    Add binned PDFs for combine

--- src/Linkdef.h
+++ src/Linkdef.h
@@ -10,9 +10,11 @@
 
 #pragma link C++ defined_in "RooRazor2DSignal.h";
 #pragma link C++ defined_in "RooRazor3DSignal.h";
+#pragma link C++ defined_in "RooRazor3DBinSignal.h";
 #pragma link C++ defined_in "RooRazor2DTail.h";
 #pragma link C++ defined_in "RooRazor2DTail_SYS.h";
 #pragma link C++ defined_in "RooRazorLShape.h";
+#pragma link C++ defined_in "RooRazor3DBinPdf.h";
 #pragma link C++ defined_in "RooBTagMult.h";
 #pragma link C++ defined_in "VecbosBtagPdf.h";
 #pragma link C++ defined_in "StandardHypoTestInvDemo.h";
